b760ef378a9198d741544b2e4f4a09920dcc519d
juliansantamaria0 <santamariajulian856@gmail.com>
2026-03-02 20:10:00 -0500

    feat: :sparkles: se agrego mas pruebas

--- sql/Script_DDL_DML.sql
+++ sql/Script_DDL_DML.sql
@@ -434,3 +434,5 @@ SELECT fn_deuda_pendiente_arriendo(2);
 SELECT fn_total_disponibles_por_tipo('CASA');
 SELECT * FROM audit_contrato;
 SELECT * FROM audit_propiedad_estado;
+SELECT * 
+FROM reporte_morosidad_mensual;
